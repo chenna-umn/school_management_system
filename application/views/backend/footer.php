4ceb34d31abe68abae91953472314f096163f01c
chennakeshav <chinna1754@gmail.com>
2018-06-04 14:38:46 -0500

    WIP

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2016 <a href="http://digitalteacher.in"target="_blank" <b> Digital Teacher Edutech Pvt. Ltd.</a> </b>
+ Developed by 
+	<a href="http://codeandpixels.net" 
+    	target="_blank">Code and Pixels</a>
+</footer>
